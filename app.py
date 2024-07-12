799d3ae432b900b7dce681152a2c00e46c3cba8e
ouola <57883444+ouola@users.noreply.github.com>
2024-07-12 15:42:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -81,5 +81,5 @@ def welcome(event):
         
 import os
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8000))
     app.run(host='0.0.0.0', port=port)
